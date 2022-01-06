52628557c5eabfee131fc39cce72318783356687
Casey Stangel <v-rogsta@microsoft.com>
2022-01-06 11:07:19 -0500

    setting up for test run

--- src/index.ts
+++ src/index.ts
@@ -21,24 +21,23 @@ async function createCommentIfError(validationResults: MdFileValidationResult) {
 }
 
 async function run() {
-    const prId = 2;
-    // const repoOwner = core.getInput('repo-owner', { required: true });
-    // const repoName = core.getInput('repo-name', { required: true });
-    // const token = core.getInput('github-token', { required: true });
-    // const requiredMetadataTags = core.getInput('required-metadata', { required: true }).split(',');
-    const requiredMetadataTags: Array<string> = ['title', 'description', 'ms.date', 'ms.topic', 'ms.prod', 'ms.custom', 'ft.audience'];
+    const requiredMetadataTags = core.getInput('required-metadata', { required: true }).split(',');
+    const repoOwner = core.getInput('repo-owner', { required: true });
+    const repoName = core.getInput('repo-name', { required: true });
+    const token = core.getInput('github-token', { required: true });
+    const prId = github.context.issue.number;
 
-    const reviewer = new MetadataReviewer("kforsberg", "pr-block-test", "" /**"PERSONAL ACCESS TOKEN"**/, axios);
+    // const requiredMetadataTags: Array<string> = ['title', 'description', 'ms.date', 'ms.topic', 'ms.prod', 'ms.custom', 'ft.audience'];
+    // const reviewer = new MetadataReviewer("kforsberg", "pr-block-test", "" /**"PERSONAL ACCESS TOKEN"**/, axios);
 
+    const reviewer = new MetadataReviewer(repoOwner, repoName, token, axios);
     const files = await reviewer.getPullRequestFiles(prId);
     const validationResults = reviewer.checkRequiredTagsForFiles(files, requiredMetadataTags);
-
     const comment = await createCommentIfError(validationResults);
 
     if (comment.length > 0) {
         reviewer.submitPullRequestReview(prId, comment);
     }
-
 }
 
 run();
